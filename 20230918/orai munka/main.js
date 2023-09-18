24d198c5debc6235546d355d28b15aaa55c7e657
szpatrik0528 <124908897+szpatrik0528@users.noreply.github.com>
2023-09-18 09:17:15 +0200

    Add files via upload

---
+++ 20230918/orai munka/main.js
@@ -0,0 +1,12 @@
+var _var1 = 1;
+let _let1 = 2;
+const _const1 = 3; 
+
+if (true) {
+    _var1 = 4;
+    _let1 = 5;
+    let _let2 = 7;
+    // _const1 = 3;
+}
+
+console.log(_var1, _let1, _const1, _let2);
