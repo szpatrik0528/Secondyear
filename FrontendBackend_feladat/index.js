39f024c728eb1c7662f5a90c0fd0de57db9ec0bd
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-11-26 22:11:58 +0100

    main

--- FrontendBackend_feladat/index.js
+++ FrontendBackend_feladat/index.js
@@ -1,28 +1,56 @@
+const mysql = require('mysql');
 const express = require('express');
-const path = require('path');
 const app = express();
 
-const mysql = require('mysql');
-const db = mysql.createConnection(
-    {
-    host: 'localhost',
-    user: 'root',
-    password: '', 
-    database: 'tagdij'
-    }
-);
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected');
+// MySQL adatbázis csatlakozás konfigurációja
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root', // A saját MySQL felhasználóneved
+  password: '', // A saját jelszavad
+  database: 'tagdij', // Az adatbázis neve, amiben dolgozol
 });
 
-app.get('/', (req, res) => {
-    const filePath = path.resolve(__dirname, './index.html');
-    res.sendFile(filePath);
+// Adatbázis csatlakozás
+connection.connect((err) => {
+  if (err) {
+    console.error('Hiba a MySQL csatlakozás során: ' + err.stack);
+    return;
+  }
+  console.log('Sikeresen csatlakozva a MySQL-hez az azonosító: ' + connection.threadId);
 });
 
+// Új ügyfél hozzáadása
+app.post('/ugyfel', (req, res) => {
+  const { name, email } = req.body;
+  const INSERT_CUSTOMER_QUERY = `INSERT INTO ugyfel (azon, nev, szulev, irszam, orsz) VALUES (?, ?, ?, ?, ?)`;
+  connection.query(INSERT_CUSTOMER_QUERY, [azon, nev, szulev, irszam, orsz], (error, results, fields) => {
+    if (error) throw error;
+    res.send('Új ügyfél hozzáadva az adatbázishoz');
+  });
+});
 
+// Összes ügyfél lekérdezése
+app.get('/ugyfel', (req, res) => {
+  const SELECT_ALL_CUSTOMERS_QUERY = 'SELECT * FROM ugyfel';
+  connection.query(SELECT_ALL_CUSTOMERS_QUERY, (error, results, fields) => {
+    if (error) throw error;
+    res.json(results);
+  });
+});
 
-app.listen(1337, () =>{
-    console.log('server is running!')
+// Egy adott ügyfél lekérdezése
+app.get('/ugyfel/:azon', (req, res) => {
+  const { customerId } = req.params;
+  const SELECT_SINGLE_CUSTOMER_QUERY = 'SELECT * FROM ugyfel WHERE azon = ?';
+  connection.query(SELECT_SINGLE_CUSTOMER_QUERY, [azon], (error, results, fields) => {
+    if (error) throw error;
+    res.json(results[0]);
+  });
+});
+
+// Egyéb kezelések...
+
+// Szerver figyelése
+app.listen(3000, () => {
+  console.log('A szerver fut a 3000-es porton');
 });
