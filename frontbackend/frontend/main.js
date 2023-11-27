70b8ea707574bcb43d509bdba858b488ff08b168
szpatrik0528 <szpatrik.photoshop@gmail.com>
2023-11-27 07:50:07 +0100

    main

--- frontbackend/frontend/main.js
+++ frontbackend/frontend/main.js
@@ -1,3 +1,34 @@
+async function getAllUsers() {
+  let endpoint = "https://retoolapi.dev/Hfa9uy/data";
+  try {
+      const response = await fetch(endpoint);
+      const users = await response.json();
+      showAllUsers(users);    
+  } catch (error) {
+      console.log(error);
+  }
+  
+}
+
+function showAllUsers() {
+  let html = "";
+    users.forEach(user => {
+        html += `
+        <div class="card" style="width: 18rem;">
+        <img src="noimage.jpg" class="card-img-top" alt="noimage.jpg">
+        <div class="card-body">
+            <h5 class="card-title">${user.id}.${user.username}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                card's content.</p>
+            <p class="card-text">Darab: ${user.darab}</p>
+            <button class="btn btn-primary" onclick="betoltInputMezobe(${user.id})">Kiválaszt</button>
+        </div>
+    </div>
+        `;
+    });
+    cards.innerHTML = html;
+};
+
 // AJAX GET kérés az összes ügyfél lekérdezésére
 fetch('/clients')
   .then(response => response.json())
